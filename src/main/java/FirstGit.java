25717b6becb1579a1d8410eada65269c72bdc0d1
IvanKonivets <groavlg@gmail.com>
2025-07-15 22:50:56 +0300

    Update FirstGit.java
    Add symbol !!! to sout FirstGit

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -1,6 +1,6 @@
 public class FirstGit {
     public static void main(String[] args) {
         System.out.println("Git");
-        System.out.println("Hello");
+        System.out.println("Hello!!!");
     }
 }
