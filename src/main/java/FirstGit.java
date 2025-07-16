e86b86c0a850d24ec5eda5e1ac05b92869d3ca30
IvanKonivets <groavlg@gmail.com>
2025-07-16 22:07:39 +0300

    change sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,7 +2,7 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("Git develop");
         System.out.println("Hello!!!");
-        System.out.println("Privet");
+        System.out.println("Privet Moscow");
         System.out.println("World");
         System.out.println("!!!!!");
     }
