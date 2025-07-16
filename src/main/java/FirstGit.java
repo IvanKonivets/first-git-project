7d74f21d0ca8a32c92b4d868948a115acf04e0d5
IvanKonivets <groavlg@gmail.com>
2025-07-16 22:06:13 +0300

    del sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,7 +2,6 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("Git develop");
         System.out.println("Hello!!!");
-        System.out.println("Privet");
         System.out.println("World");
         System.out.println("!!!!!");
     }
